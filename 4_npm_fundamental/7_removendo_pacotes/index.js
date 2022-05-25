5c1257c6e9498951e62b4e387f693a8781ce3fe3
321-bip <dominicmarcos80@gmail.com>
2022-05-24 21:08:45 -0300

    sesao 4 enserrada

---
+++ 4_npm_fundamental/7_removendo_pacotes/index.js
@@ -0,0 +1,9 @@
+const chalk = require( 'chalk' );
+const _ = require( 'lodash');
+
+const a = [ 1, 8, 9, 10];
+const b = [ 7, 5, 8, 1];
+
+const diferença = _.difference(b, a)
+
+console.log( chalk.red.bold( diferença ) );
